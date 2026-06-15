394566d2267aa0301e34374676d750c1aa928c84
kaifkk <https://kaifkk@github.com>
2026-06-15 20:57:32 +0600

    lcm

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 4000;
+
+function gcd(a, b) {
+  while(b !== 0n) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
+
+function parseNatural(value) {
+  if (typeof value !== 'string') {
+    return null;
+  }
+
+  const n = BigInt(value);
+
+  if(n <= 0n) {
+    return null;
+  }
+
+  return n;
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/kkaif0035_gmail_com/lcm', (req, res) => {
+  const x = parseNatural(req.query.x);
+  const y = parseNatural(req.query.y);
+
+  if(x === null || y === null) {
+    return res.type('text/plain').send("NaN");
+  }
+
+  const lcm = (x * y) / gcd(x, y);
+
+  res.type('text/plain').send(String(lcm));
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
